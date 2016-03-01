6a3ee3e3bac52d05b630684d24572a9f5d20f91f
Shane Hussel <shussel@gmail.com>
2016-02-29 21:56:16 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+var app = angular.module('dogs', []);
+
+app.controller('dogsController', function($scope, $http) {
+	'use strict';
+
+    $scope.dogs = [];
+	$http.get('dogs.json')
+		.success(function(data) {
+       $scope.dogs = data;
+	}).
+		error(function() {
+			alert('error');
+	});
+});
