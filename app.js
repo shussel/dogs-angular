f5ef1c468775a7f95cdbfe11f8d8ffa1ad9a7413
Shane Hussel <shussel@gmail.com>
2016-03-02 17:07:20 -0500

    move dog list to service

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
 var app = angular.module('dogs', []);
 
-app.controller('dogsController', function($scope, $http) {
+app.controller('dogsController', function($scope, dogsService) {
 	'use strict';
-
-    $scope.dogs = [];
-	$http.get('dogs.json')
-		.success(function(data) {
-       $scope.dogs = data;
-	}).
-		error(function() {
-			alert('error');
-	});
 
 	$scope.sortType     = 'id';
 	$scope.sortReverse  = false;
+    dogsService.getDogs().then(function(data) {
+		$scope.dogs = data;
+	}).catch(function() {
+		$scope.error = 'unable to get the dogs';
+	});
+	
+});
+
+app.service('dogsService', function($http, $q) {
+
+	this.getDogs = function() {
+
+		return $http.get('dogs.json').then(function(response) {
+					return response.data;
+			   });
+	}
 });
