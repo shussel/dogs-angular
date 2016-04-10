c240457b313039cd4f89b0a4dd56351a3bf3d9a5
Shane Hussel <shussel@gmail.com>
2016-04-10 12:51:10 -0400

    switch to foundation for apps

---
+++ client/assets/js/app.js
@@ -0,0 +1,98 @@
+(function() {
+  'use strict';
+
+  angular.module('application', [
+    'ui.router',
+    'ngAnimate',
+
+    //foundation
+    'foundation',
+    'foundation.dynamicRouting',
+    'foundation.dynamicRouting.animations'
+  ])
+	.controller('dogsController', function($scope, dogsService) {
+
+		$scope.columns = ['ID','Name','Breed','Color','Weight','Owner'];
+		
+		$scope.sort = {
+			'column': 'ID',
+			'reverse': false
+		}
+
+		dogsService.getDogs().then(function(data) {
+			$scope.dogs = data;
+		}).catch(function() {
+			$scope.error = 'unable to get the dogs';
+		});
+		
+	})
+
+	.service('dogsService', function($http) {
+
+		this.getDogs = function() {
+
+			return $http.get('assets/data/dogs.json').then(function(response) {						
+						return response.data;
+				   });
+		}
+	})
+
+	.directive('sortHeader', function() {
+	  return {
+		restrict: 'A',
+		scope: {
+			column: '@',
+			sort: '='
+		},
+		templateUrl: '/templates/sortHeader.html',
+		bindToController: true,
+		controllerAs: "ctrl",
+		controller: SortHeaderController
+	  };
+	})
+
+	.directive('sortHeaders', function() {
+	  return {
+		restrict: 'A',
+		scope: {
+			columns: '=',
+			sort: '='
+		},
+		templateUrl: '/templates/sortHeaders.html',
+		bindToController: true,
+		controllerAs: "ctrl",
+		controller: SortHeadersController
+	  };
+	})
+
+    .config(config)
+    .run(run)
+  ;
+
+  config.$inject = ['$urlRouterProvider', '$locationProvider'];
+
+  function SortHeaderController() {
+	this.toggleSort = function() {
+		this.sort.column = this.column;
+		this.sort.reverse = !this.sort.reverse;
+	};
+  }
+
+  function SortHeadersController() {}
+
+  function config($urlProvider, $locationProvider) {
+    $urlProvider.otherwise('/');
+
+    $locationProvider.html5Mode({
+      enabled:false,
+      requireBase: false
+    });
+
+    $locationProvider.hashPrefix('!');
+  }
+
+  function run() {
+    FastClick.attach(document.body);
+  }
+
+})();
